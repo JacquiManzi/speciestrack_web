2b60ce41397428bab1979787015e65b383e288bc
Jacqui Manzi <jacquimanzi@gmail.com>
2025-10-31 13:04:09 -0700

    Drawing Wildcat Canyon Regional park on the map, also installing Prettier for formatting

--- App.tsx
+++ App.tsx
@@ -33,8 +33,8 @@ const mapHTML = `
     <div id="map"></div>
     <script>
         // Wildcat Canyon Regional Park coordinates
-        const longitude = -122.3094;
-        const latitude = 37.9119;
+        const longitude = -122.3138;
+        const latitude = 37.94836;
 
         // Create map
         const map = new ol.Map({
@@ -50,31 +50,32 @@ const mapHTML = `
             })
         });
 
-        // Add a marker for Wildcat Canyon Regional Park
-        const marker = new ol.Feature({
-            geometry: new ol.geom.Point(ol.proj.fromLonLat([longitude, latitude]))
+        const wkt = 'POLYGON((-122.28112 37.91874,-122.27067 37.92392,-122.27061 37.92138,-122.26765 37.92143,-122.262 37.92416,-122.2659 37.93392,-122.27042 37.93614,-122.28178 37.94702,-122.28391 37.9473,-122.28559 37.95072,-122.29028 37.95304,-122.28642 37.95197,-122.28435 37.95408,-122.29229 37.95429,-122.2975 37.95679,-122.29822 37.95575,-122.29613 37.95525,-122.29899 37.95366,-122.30203 37.95487,-122.30175 37.95264,-122.30828 37.95267,-122.30794 37.96,-122.31055 37.96004,-122.31557 37.9594,-122.31875 37.95404,-122.3244 37.95385,-122.32226 37.95131,-122.3163 37.95097,-122.31596 37.94868,-122.3138 37.94836,-122.31248 37.94682,-122.31136 37.94882,-122.30721 37.9454,-122.31131 37.9456,-122.31168 37.94403,-122.3101 37.94503,-122.29522 37.93138,-122.29224 37.93069,-122.29064 37.92924,-122.2918 37.92726,-122.28112 37.91874),(-122.31321 37.95783,-122.31039 37.95636,-122.31337 37.95701,-122.31321 37.95783))';
+
+        const format = new ol.format.WKT();
+        const wildCatFeature = format.readFeature(wkt, {
+            dataProjection: 'EPSG:4326',   // projection of your WKT coordinates
+            featureProjection: 'EPSG:3857' // projection of your map view
         });
 
-        const markerStyle = new ol.style.Style({
-            image: new ol.style.Circle({
-                radius: 8,
-                fill: new ol.style.Fill({ color: '#ff4444' }),
-                stroke: new ol.style.Stroke({
-                    color: '#ffffff',
-                    width: 2
-                })
-            })
-        });
+        const wildCatStyle = new ol.style.Style({
+            stroke: new ol.style.Stroke({
+            color: 'rgba(255, 0, 0, 1)',  // red border
+            width: 2
+        }),
+        fill: new ol.style.Fill({
+          color: 'rgba(255, 0, 0, 0.3)' // semi-transparent red fill
+        })
+      });
 
-        marker.setStyle(markerStyle);
+        const wildCatVectorLayer = new ol.layer.Vector({
+          source: new ol.source.Vector({
+          features: [wildCatFeature]
+          }),
+          style: wildCatStyle
+      });
 
-        const vectorLayer = new ol.layer.Vector({
-            source: new ol.source.Vector({
-                features: [marker]
-            })
-        });
-
-        map.addLayer(vectorLayer);
+        map.addLayer(wildCatVectorLayer);
     </script>
 </body>
 </html>
